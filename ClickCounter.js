8339a59fef6a9deb15f3ce20526a65f99db9b87d
jialingWei <390465032@qq.com>
2017-11-20 23:58:30 +0800

    fist react

---
+++ ClickCounter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Appone extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>我4488</p>
+      </div>
+    );
+  }
+}
+
+export default Appone;
---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import ControlPanel from './ControlPanel';
+
+
+ReactDOM.render(<ControlPanel />, document.getElementById('roottwo'));
+
